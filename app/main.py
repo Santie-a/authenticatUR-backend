3cc40661f3ea82fedc7584b1169e648d4f989f65
Santie-a <diaz.obando.santiago@gmail.com>
2025-03-02 11:07:52 -0500

    Changed cors policy

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS settings (adjust in production)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:8000", settings.FRONTEND_URL],  # React frontend URL
+    allow_origins=[settings.FRONTEND_URL],  # React frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
